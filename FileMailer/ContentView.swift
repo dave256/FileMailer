a21442bd6e97c69680530dee58b33d90cac7b908
Dave Reed <dave256@mac.com>
2020-09-06 12:14:18 -0400

    Initial Commit

---
+++ FileMailer/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  FileMailer
+//
+//  Created by David M Reed on 9/6/20.
+//  Copyright © 2020 David M Reed. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
